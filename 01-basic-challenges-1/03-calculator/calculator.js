351c1e7b1ac8613d02d0c5ad07f28c93636977b3
Israel Santana <is.wsie@gmail.com>
2025-04-01 14:24:35 -0400

    Learned:
    - Use if / else if / else when you need to evaluate different, potentially complex boolean conditions, check ranges, or compare multiple variables.
    
    -Use switch when you are comparing a single variable or expression against a set of specific, discrete values.

--- 01-basic-challenges-1/03-calculator/calculator.js
+++ 01-basic-challenges-1/03-calculator/calculator.js
@@ -1,3 +1,44 @@
-function calculator() {}
+function calculator(num1, num2, operator) {
+  //  switch statement version:
+  let result;
+
+  switch (operator) {
+    case "+":
+      result = num1 + num2;
+      break;
+    case "-":
+      result = num1 - num2;
+      break;
+    case "*":
+      result = num1 * num2;
+      break;
+    case "/":
+      result = num1 / num2;
+      break;
+
+    default:
+      throw new Error("Invalid operator");
+  }
+
+  return result;
+}
 
 module.exports = calculator;
+
+/* if statement version:
+
+  let result;
+
+  if (operator === "+") {
+    result = num1 + num2;
+  } else if (operator === "-") {
+    result = num1 - num2;
+  } else if (operator === "*") {
+    result = num1 * num2;
+  } else if (operator === "/") {
+    result = num1 / num2;
+  } else {
+    throw new Error("Invalid operator");
+  }
+
+  return result; */
