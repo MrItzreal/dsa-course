6218d7a76b07eef12b235c67f620bc858075493d
Israel Santana <is.wsie@gmail.com>
2025-04-02 12:32:39 -0400

    Implement findMaxNumber function and add corresponding tests

--- 01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,3 +1,27 @@
-function findMaxNumber() {}
+function findMaxNumber(arr) {
+  let max = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+  return max;
+}
 
 module.exports = findMaxNumber;
+
+/* .find solution:
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let findNumber = numbers.find((number) => number === 10);
+
+console.log(findNumber)
+*/
+
+/* Math.max solution:
+
+function findMaxNumber(arr) {
+  return Math.max(...arr);
+}
+  */
