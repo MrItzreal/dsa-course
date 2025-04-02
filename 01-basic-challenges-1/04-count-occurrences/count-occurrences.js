ad28b71cb1832bfc4517681f0c137fd38f333ad6
Israel Santana <is.wsie@gmail.com>
2025-04-02 09:21:29 -0400

    Learned about .split():
    -Which takes a string, cuts it up based on the separator you provide, and gives you back an array containing the pieces.

--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,3 +1,16 @@
-function countOccurrences() {}
+// For loop solution:
+function countOccurrences(str, char) {
+  let count = 0;
 
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === char) {
+      count++;
+    }
+  }
+
+  return count;
+}
+
+/* .split solution
+const countOccurrences = (str, char) => str.split(char).length - 1;*/
 module.exports = countOccurrences;
