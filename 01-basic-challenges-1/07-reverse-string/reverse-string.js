6680125d621cf6c07748178079d701e8362f2f37
Israel Santana <is.wsie@gmail.com>
2025-04-05 09:41:49 -0400

    got more practice with for loops and different js methods

--- 01-basic-challenges-1/07-reverse-string/reverse-string.js
+++ 01-basic-challenges-1/07-reverse-string/reverse-string.js
@@ -1,3 +1,19 @@
-function reverseString() {}
+function reverseString(str) {
+  let reversed = "";
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed += str[i];
+  }
+
+  return reversed;
+}
+
+/* 
+built-in methods solution:
+return str.split("").reverse().join("");
+
+arrow function solution:
+const reverseString = (str) => str.split("").reverse().join("");
+*/
 
 module.exports = reverseString;
