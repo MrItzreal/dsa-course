75c88cdfd76b00aeb5fa6581dd0b0fb094e25c16
Israel Santana <is.wsie@gmail.com>
2025-04-09 10:45:26 -0400

    Learned that the 'Set\' method only stores unique values

--- 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
+++ 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
@@ -1,3 +1,16 @@
-function removeDuplicates() {}
+function removeDuplicates(arr) {
+  // 'for loop' solution:
+  const uniqueArr = [];
 
+  for (let i = 0; i < arr.length; i++) {
+    if (!uniqueArr.includes(arr[i])) {
+      uniqueArr.push(arr[i]);
+    }
+  }
+  return uniqueArr;
+}
+
+/* 'Set' solution:
+  return [...new Set(arr)];
+*/
 module.exports = removeDuplicates;
