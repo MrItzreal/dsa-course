d30a21a208773afccdfa42b1828209fd95f04539
Israel Santana <is.wsie@gmail.com>
2025-04-16 11:33:10 -0400

    Learned about the .reduce() method which takes an array and reduces it down to a single value

--- 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
+++ 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
@@ -1,3 +1,19 @@
-function findMissingNumber() {}
+function findMissingNumber(arr) {
+  if (arr.length === 0) return 1;
+
+  const n = arr.length + 1;
+  const expectSum = (n * (n + 1)) / 2;
+
+  /* for loop solution:
+   let calculatedSum = 0;
+   for (let i = 0; i < arr.length; i++) {
+     calculatedSum += arr[i];
+   return expectSum - calculatedSum;
+   */
+
+  // .reduce solution:
+  const actualSum = arr.reduce((sum, num) => sum + num, 0);
+  return expectSum - actualSum;
+}
 
 module.exports = findMissingNumber;
