4700164011f74537fe75dbebbf99d64ae56ed3f0
Israel Santana <is.wsie@gmail.com>
2025-04-17 15:21:11 -0400

    Got more practice using the .set method)

--- 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
+++ 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
@@ -1,3 +1,34 @@
-function areAllCharactersUnique() {}
+function areAllCharactersUnique(str) {
+  //Using a Set solution:
+  // This is the current character
+  const charSet = new Set();
+
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+    if (charSet.has(char)) {
+      return false;
+    }
+
+    charSet.add(char);
+  }
+
+  return true;
+
+  /* Using an object solution
+  
+  function areAllCharactersUnique(str) {
+  const charCount = {};
+
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+    if (charCount[char]) {
+      return false;
+    }
+    charCount[char] = true;
+  }
+
+  return true;
+}*/
+}
 
 module.exports = areAllCharactersUnique;
